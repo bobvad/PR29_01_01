c30ad3c32f4ca43bcc2577966653d866a29ec277
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-09-30 01:00:56 +0500

    Разработанный модуль управления клубами. Дегтянников

---
+++ PR29_Degtinnikov/Pages/Clubs/Main.xaml.cs
@@ -0,0 +1,35 @@
+﻿using PR29_Degtinnikov.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PR29_Degtinnikov.Pages.Clubs
+{
+    /// <summary>
+    /// Логика взаимодействия для Main.xaml
+    /// </summary>
+    public partial class Main : Page
+    {
+        public ClubsContext AllClub = new ClubsContext();
+        public Main()
+        {
+            InitializeComponent();
+            foreach (Models.Clubs Club in AllClubs.Clubs)
+                Parent.Children.Add(new Elements.Item());
+        }
+
+        private void AddClub(object sender, RoutedEventArgs e)
+        => MainWindow.init.OpenPages(new Pages.Clubs.Add(this));
+    }
+}
