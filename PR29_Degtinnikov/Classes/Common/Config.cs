3f1a6395882caea0bd0ca8aaa863fe75f074238e
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-09-29 22:54:30 +0500

    Разработанные контексты для моделей. Дегтянников.

---
+++ PR29_Degtinnikov/Classes/Common/Config.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR29_Degtinnikov.Classes.Common
+{
+    public class Config
+    {
+        public static string ConnectionConfig = "server=";
+        public static MySqlServerVersion Version = new MySqlServerVersion(new Version(8,0,11));
+    }
+}
---
+++ PR29_Degtinnikov/Classes/UserContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PR29_Degtinnikov.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR29_Degtinnikov.Classes
+{
+    public class UserContext:DbContext
+    {
+        public DbSet<Models.Users> Users { get; set; }
+        public UserContext() 
+        {
+            Database.EnsureCreated();
+            Users.Load();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(Common.Config.ConnectionConfig, Common.Config.Version);
+        }
+    }
+}
